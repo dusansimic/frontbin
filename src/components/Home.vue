c09ffb3653f68c82120bbc730b06ee83ac48b07f
Dušan Simić <dusan.simic1810@gmail.com>
2018-01-15 11:13:11 +0100

    Added all files

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="Home">
+		<AceEditor :theme="github" :options="editorOptions" :lang="javascript" style="font-size: 16px"/>
+	</div>
+</template>
+
+<script>
+import AceEditor from 'vue2-ace'
+import 'brace/mode/javascript'
+import 'brace/theme/github'
+
+export default {
+	name: 'Home',
+	components: {
+		AceEditor
+	},
+	data () {
+		return {
+			editorOptions: {
+				fontSize: '16px'
+			}
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
